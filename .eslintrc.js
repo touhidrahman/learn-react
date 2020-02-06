2a07f1465e0a22b8a3b185c4b76d6c797d50ddd0
Touhidur Rahman <touhidrahman1987@gmail.com>
2020-02-06 23:05:36 +0100

    Remove warnings by using more config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,25 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     extends: [
+        'plugin:react/recommended',
         'plugin:@typescript-eslint/recommended',
-        'react-app',
+        'prettier/@typescript-eslint',
         'plugin:prettier/recommended',
     ],
+    parserOptions: {
+        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
+        ecmaFeatures: {
+            jsx: true, // Allows for the parsing of JSX
+        },
+    },
     plugins: ['@typescript-eslint', 'react'],
-    rules: {},
+    rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+    settings: {
+        react: {
+            version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+        },
+    },
 }
