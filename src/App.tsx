b591660f34e77851c7aec44783d10cc8db532dca
Touhidur Rahman <touhidrahman1987@gmail.com>
2020-02-07 22:08:17 +0100

    Function life cycle (page 145)

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,88 @@ import logo from './logo.svg'
 import './App.css'
 import Confirm from './Confirm'
 
-const App = () => {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-            </header>
-            <Confirm title="A confirm modal" content="Are you sure?" />
-        </div>
-    )
+interface IState {
+    confirmOpen: boolean
+    confirmMessage: string
+    confirmVisible: boolean
+    countDown: number
+}
+
+class App extends React.Component<{}, IState> {
+    private timer = 0
+
+    constructor(props: {}) {
+        super(props)
+
+        this.state = {
+            confirmOpen: false,
+            confirmMessage: 'Please hit the confirm button',
+            confirmVisible: true,
+            countDown: 10,
+        }
+    }
+
+    componentDidMount() {
+        this.timer = window.setInterval(() => this.handleTimerTick(), 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer)
+    }
+
+    private handleTimerTick() {
+        this.setState(
+            {
+                confirmMessage: `Please hit the confirm button in ${this.state.countDown} seconds`,
+                countDown: this.state.countDown - 1,
+            },
+            () => {
+                if (this.state.countDown <= 0) {
+                    clearInterval(this.timer)
+                    this.setState({
+                        confirmMessage: 'Too late to confirm',
+                        confirmVisible: false,
+                    })
+                }
+            },
+        )
+    }
+
+    private handleOkClick = () => {
+        this.setState({ confirmOpen: false, confirmMessage: 'You have clicked OK' })
+        clearInterval(this.timer)
+    }
+
+    private handleCancelClick = () => {
+        this.setState({ confirmOpen: false, confirmMessage: 'You have canceled' })
+        clearInterval(this.timer)
+    }
+
+    private handleConfirmClick = () => {
+        this.setState({ confirmOpen: true })
+        clearInterval(this.timer)
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </header>
+                <p>{this.state.confirmMessage}</p>
+                {this.state.confirmVisible && (
+                    <button onClick={this.handleConfirmClick}>Open Modal</button>
+                )}
+                <Confirm
+                    open={this.state.confirmOpen}
+                    title="A confirm modal"
+                    content="Are you sure?"
+                    onOkClick={this.handleOkClick}
+                    onCancelClick={this.handleCancelClick}
+                />
+            </div>
+        )
+    }
 }
 
 export default App
--- src/Confirm.tsx
+++ src/Confirm.tsx
@@ -4,48 +4,51 @@ import './Confirm.css'
 interface IProps {
     title: string
     content: string
+    open: boolean
     cancelCaption?: string
     okCaption?: string
-    // onOkClick: () => void
-    // onCancelClick: () => void
+    onOkClick: () => void
+    onCancelClick: () => void
 }
 
-class Confirm extends React.Component<IProps> {
-    static defaultProps = {
-        cancelCaption: 'Cancel',
-        okCaption: 'OK',
+const Confirm: React.SFC<IProps> = (props) => {
+    const [cancelClickCount, setCancelClickCount] = React.useState(0)
+
+    const handleOkClick = () => {
+        props.onOkClick()
     }
 
-    private handleOkClick = () => {
-        // this.props.onOkClick()
+    const handleCancelClick = () => {
+        const newCount = cancelClickCount + 1
+        setCancelClickCount(newCount)
+        if (newCount >= 2) props.onCancelClick()
     }
 
-    private handleCancelClick = () => {
-        // this.props.onCancelClick()
-    }
-
-    render() {
-        return (
-            <div className="confirm-wrapper confirm-visible">
-                <div className="confirm-container">
-                    <div className="confirm-title-container">
-                        <span>{this.props.title}</span>
-                    </div>
-                    <div className="confirm-content-container">
-                        <p>{this.props.content}</p>
-                    </div>
-                    <div className="confirm-buttons-container">
-                        <button className="confirm-cancel" onClick={this.handleCancelClick}>
-                            {this.props.cancelCaption}
-                        </button>
-                        <button className="confirm-ok" onClick={this.handleOkClick}>
-                            {this.props.okCaption}
-                        </button>
-                    </div>
+    return (
+        <div className={props.open ? 'confirm-wrapper confirm-visible' : 'confirm-wrapper'}>
+            <div className="confirm-container">
+                <div className="confirm-title-container">
+                    <span>{props.title}</span>
+                </div>
+                <div className="confirm-content-container">
+                    <p>{props.content}</p>
+                </div>
+                <div className="confirm-buttons-container">
+                    <button className="confirm-cancel" onClick={handleCancelClick}>
+                        {cancelClickCount === 0 ? props.cancelCaption : 'Really?'}
+                    </button>
+                    <button className="confirm-ok" onClick={handleOkClick}>
+                        {props.okCaption}
+                    </button>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+}
+
+Confirm.defaultProps = {
+    cancelCaption: 'Cancel',
+    okCaption: 'OK',
 }
 
 export default Confirm
